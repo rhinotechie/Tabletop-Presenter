99ac815ac34a636ed1f19cd87e52b1439c632c65
Ryan <18726598+rhinotechie@users.noreply.github.com>
2022-07-05 12:27:24 -0700

    Created a main class since JavaFX doesn't like a class that extends Application as the entrypoint. Maven build makes a fatjar.

---
+++ src/main/java/com/ryanaong/tabletoppresenter/MainClass.java
@@ -0,0 +1,8 @@
+package com.ryanaong.tabletoppresenter;
+
+// App entry point
+public class MainClass {
+    public static void main(String[] args) {
+        TableTopApplication.main(args);
+    }
+}
